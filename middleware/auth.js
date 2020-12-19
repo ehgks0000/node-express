eae45e2791fc673add47f46a332d6bd6ec58234c
ehgks0000 <ehgks0083@gmail.com>
2020-12-19 20:23:04 +0900

    미들웨어 auth 재수정

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,26 +3,40 @@ const User = require('../models/Users');
 const auth = (req, res, next) => {
   const token = req.cookies.x_auth;
   if (!token) {
-    // console.log('auth : 로그인 안되어 있습니다!');
-    // res.status(500).send('auth : 로그인 안되어 있습니다!');
-    // throw new Error('please authenticate');
     next();
   }
-  try {
-    User.findByToken(token, process.env.JWT_SECRET_KEY3).then(user => {
+
+  User.findByToken(token, process.env.JWT_SECRET_KEY3)
+    .then(user => {
       if (!user) {
         console.log('auth : 로그인 안되어 있습니다!');
-        res.clearCookie('x_auth');
+        return res.clearCookie('x_auth');
       }
       req.token = token;
       req.user = user;
       next();
+    })
+    .catch(err => {
+      throw new Error('please authenticate');
     });
-  } catch (e) {
-    // res.clearCookie('x_auth');
-    throw new Error('please authenticate');
-    // res.status(401).send({ error: 'please authenticate.' });
-  }
 };
 
 module.exports = { auth };
+// if (!token) {
+//     next();
+//   }
+//   try {
+//     User.findByToken(token, process.env.JWT_SECRET_KEY3).then(user => {
+//       if (!user) {
+//         console.log('auth : 로그인 안되어 있습니다!');
+//         res.clearCookie('x_auth');
+//       }
+//       req.token = token;
+//       req.user = user;
+//       next();
+//     });
+//   } catch (e) {
+//     // res.clearCookie('x_auth');
+//     throw new Error('please authenticate');
+//     // res.status(401).send({ error: 'please authenticate.' });
+//   }
